5abd6c552b906dddbf6e02a57c35058a07b3258c
RSepulvedaDev10 <94391613+RSepulvedaDev10@users.noreply.github.com>
2022-01-04 15:38:48 -0600

    Create Dev10 M07 - Data Classes.py

---
+++ Dev10 M07 - Data Classes.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass, field
+
+class Orders:
+    @dataclass
+    
+    
+    def __gt__(self, other):
+        pass
+    
+    def __ge__(self, other):
+        pass
+
+class Invoices:
+    @dataclass
+    
+    
+    def __gt__(self, other):
+        pass
+    
+    def __ge__(self, other):
+        pass
+
+class Customers:
+    @dataclass
+    
+    CustomerID: int
+    CustomerName: str
+    CreditLimit: float
+    
+    def __gt__(self, other):
+        pass
+    
+    def __ge__(self, other):
+        pass
